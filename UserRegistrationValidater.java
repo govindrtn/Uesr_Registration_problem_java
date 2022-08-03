9fb26a31afede8479e7c5e9a195d2c3b7e7f8fc0
Govind maithil <govindmaithilrtn@gmail.com>
2022-08-03 22:12:05 +0530

    Added : Last name validate

--- UserRegistrationValidater.java
+++ UserRegistrationValidater.java
@@ -4,14 +4,20 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationValidater {
 
-	private static final String First_Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";
+	private static final String Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";
 
 	public static boolean firstNameValidater(String firstName) {
-		Pattern pattern = Pattern.compile(First_Name_Pattern);
+		Pattern pattern = Pattern.compile(Name_Pattern);
 		return pattern.matcher(firstName).matches();
+	}
+	
+	public static boolean lastNameValidater(String lastName) {
+		Pattern pattern = Pattern.compile(Name_Pattern);
+		return pattern.matcher(lastName).matches();
 	}
 
 	public static void main(String[] args) {
 				System.out.println(firstNameValidater("Govind"));
+				System.out.println(lastNameValidater("Maithil"));
 	}
 }
