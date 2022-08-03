30eefc2b11ae5b9268486402a77344705558531f
Govind maithil <govindmaithilrtn@gmail.com>
2022-08-03 22:31:05 +0530

    Added : phone number validate

--- UserRegistrationValidater.java
+++ UserRegistrationValidater.java
@@ -6,6 +6,7 @@ public class UserRegistrationValidater {
 
 	private static final String Name_Pattern = "^[A-Z]{1}[a-z]{2,}$";
 	private static final String Email_Pattern = "^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$";
+	private static final String Phone_Pattern = "^[0-9]{2}[ ][0-9]{10}";
 
 	public static boolean firstNameValidater(String firstName) {
 		Pattern pattern = Pattern.compile(Name_Pattern);
@@ -21,12 +22,17 @@ public class UserRegistrationValidater {
 		Pattern pattern = Pattern.compile(Email_Pattern);
 		return pattern.matcher(email).matches();
 	}
+	public static boolean phoneValidater(String phone) {
+		Pattern pattern = Pattern.compile(Phone_Pattern);
+		return pattern.matcher(phone).matches();
+	}
 
 	public static void main(String[] args) {
 		System.out.println(firstNameValidater("Govind"));
 		System.out.println(lastNameValidater("Maithil"));
 		System.out.println(emailValidater("govindmaithil@gmail.com"));
 		System.out.println(emailValidater("govindmaithil@bridgelabz.co.in"));
+		System.out.println(phoneValidater("91 8602443526"));
 	
 	}
 }
